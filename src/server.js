a54281eab3b3c2cf116f430c32f4a026a39929d7
irfanfajru <irfanfajru025@gmail.com>
2022-04-19 01:33:12 +0700

    add songs n album plugin

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const albums = require('./api/albums');
+const SongsService = require('./services/postgres/SongsService');
+const AlbumsService = require('./services/postgres/AlbumsServices');
+const SongsValidator = require('./validator/songs');
+const AlbumValidator = require('./validator/albums');
 require('dotenv').config();
 
 const init = async () => {
+  const songsService = new SongsService();
+  const albumsService = new AlbumsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -11,6 +20,24 @@ const init = async () => {
       },
     },
   });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumValidator,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+  ]);
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
