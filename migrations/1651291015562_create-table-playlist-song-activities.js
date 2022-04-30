4454849f97df69d3807ef23e281cf5a49b79c9f0
irfanfajru <irfanfajru025@gmail.com>
2022-04-30 11:14:39 +0700

    pembuatan db colab n psactivities

---
+++ migrations/1651291015562_create-table-playlist-song-activities.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('playlist_song_activities', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'playlists(id)',
+      onDelete: 'cascade',
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      referenes: 'songs(id)',
+      onDelete: 'cascade',
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users(id)',
+      onDelete: 'cascade',
+    },
+    action: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    time: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_song_activities');
+};
