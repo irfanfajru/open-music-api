ef225664f2d0b2b640e53d5af2a0e5972e8fc4a9
irfanfajru <irfanfajru025@gmail.com>
2022-05-06 13:42:01 +0700

    api album likes created

---
+++ src/api/albumlikes/handler.js
@@ -0,0 +1,76 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class AlbumLikesHandler {
+  constructor(albumsService, service) {
+    this._albumsService = albumsService;
+    this._service = service;
+
+    this.postAlbumLikesHandler = this.postAlbumLikesHandler.bind(this);
+    this.getAlbumLikesHandler = this.getAlbumLikesHandler.bind(this);
+  }
+
+  async postAlbumLikesHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const { userId } = request.auth.credentials;
+      await this._albumsService.getAlbumById(id);
+      const result = await this._service.addOrdeleteAlbumLikeIfExists(id, userId);
+      const response = h.response({
+        status: 'success',
+        message: result,
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+      // server error
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf,terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      return response;
+    }
+  }
+
+  async getAlbumLikesHandler(request, h) {
+    try {
+      const { id } = request.params;
+      await this._albumsService.getAlbumById(id);
+      const likes = await this._service.getAlbumLikes(id);
+      const response = h.response({
+        status: 'success',
+        data: {
+          likes,
+        },
+      });
+      response.code(200);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+      // server error
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf,terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      return response;
+    }
+  }
+}
+
+module.exports = AlbumLikesHandler;
--- src/api/albums/index.js
+++ src/api/albums/index.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 
 module.exports = {
   name: 'albums',
-  versioin: '1.0.0',
+  version: '1.0.0',
   register: async (server, { service, validator }) => {
     const albumHandler = new AlbumHandler(service, validator);
     server.route(routes(albumHandler));
